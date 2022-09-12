6060e6ae85f4262a8d3443082d3101c221f34310
serchemach <serchemach@gmail.com>
2022-09-12 12:12:44 +0300

    Add dotenv support Test some sample code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { Telegraf } from "telegraf";
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
 bot.command('hipster', Telegraf.reply('λ'));
+bot.command('start', (ctx) => {
+    ctx.getChat().then((chat) => {
+        const chatObjectString = JSON.stringify(chat);
+        ctx.reply(chatObjectString);
+        console.log(chatObjectString);
+    })
+})
 bot.launch();
+
+bot.telegram.sendMessage("11111", "HIIIIII!");
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
