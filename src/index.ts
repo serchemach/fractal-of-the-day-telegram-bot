6f753d384deb2800dccef87d0e78d42c2d183ade
serchemach <serchemach@gmail.com>
2022-09-12 11:58:02 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('hipster', Telegraf.reply('λ'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
